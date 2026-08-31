6c32850516d9657cb1971dc86ff6b44672d8b56c
Joshua <joshua.vivero.e@gmail.com>
2026-08-30 19:13:28 -0500

    Anadi un main y modifique algunos metodos para mejor seguimiento del VSC

--- ED-202610/src/CP/Practice01_TController.java
+++ ED-202610/src/CP/Practice01_TController.java
@@ -1,9 +1,16 @@
-package Mycode;
+package CP;
 
-public class Practice01_Tcontroller {
+import TDAs.SinglyLinkedList;
+
+public class Practice01_TController {
 Practice01_TGroup grupo ;
     public void añadirEstudiantes(Practice01_TEstud estudiante){
         grupo.getEstudiantes().addLast(estudiante);
 
     }
+
+    public String obtenerMejorAlumno(){
+        return grupo.getBestStudent();
+    }
+
 }
